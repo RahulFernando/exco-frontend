6414f9601cd3b4e5d6705c254f2df46391ec151d
RahulFernando <rahulrulz680@gmail.com>
2022-05-01 15:02:07 +0530

    feat: count down

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Alert } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+
+// context
+import AuthContext from '../../context/auth-context';
+
+// util
+import { getUserDetails, daysRemains } from '../../helpers/util';
 
 const useStyles = makeStyles({
   image: {
@@ -11,6 +17,79 @@ const useStyles = makeStyles({
 
 const CartItem = ({ item }) => {
   const classes = useStyles();
+  const { user } = useContext(AuthContext);
+
+  const [overdue, setOverdue] = useState();
+
+  let content = (
+    <Grid item xs={12}>
+      <Alert severity="error">Overdue!</Alert>
+    </Grid>
+  );
+
+  const fetchUserDetails = useCallback(async () => {
+    const { user_role } = await getUserDetails(user.token);
+
+    // if user is student and book lending
+    if (user_role === 'student' && item.type === 1) {
+      const from = new Date(item.date);
+      const to = new Date().setDate(from.getDate() + 14);
+
+      setOverdue(daysRemains(from, to));
+    }
+
+    // if user is student and book reference
+    if (user_role === 'student' && item.type === 0) {
+      const from = new Date(item.date);
+      const to = new Date().setDate(from.getDate() + 1);
+
+      setOverdue(daysRemains(from, to));
+    }
+
+    // if user is faculty member and book lending
+    if (user_role === 'faculty_member' && item.type === 1) {
+      const from = new Date(item.date);
+      const to = new Date().setDate(from.getDate() + 30);
+
+      setOverdue(daysRemains(from, to));
+    }
+
+    // if user is faculty member and book reference
+    if (user_role === 'faculty_member' && item.type === 0) {
+      const from = new Date(item.date);
+      const to = new Date().setDate(from.getDate() + 3);
+
+      setOverdue(daysRemains(from, to));
+    }
+  }, [item.date, item.type, user.token]);
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, [fetchUserDetails]);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      fetchUserDetails();
+    }, 1000);
+
+    return () => {
+      clearTimeout(id);
+    };
+  });
+
+  if (overdue?.days !== 0 && overdue?.hours !== 0) {
+    content = (
+      <>
+        <Grid item xs={3}>
+          <Typography variant="subtitle1">Overdue by</Typography>
+        </Grid>
+        <Grid item xs={3} />
+        <Grid item xs={4}>
+          <Typography variant="subtitle1">{`${overdue?.days} days ${overdue?.hours} hours`}</Typography>
+        </Grid>
+      </>
+    );
+  }
 
   return (
     <Grid container>
@@ -19,6 +98,9 @@ const CartItem = ({ item }) => {
       </Grid>
       <Grid item xs={9} pl={2}>
         <Typography variant="h6">{item.name}</Typography>
+        <Grid container item xs={12} pt={2} spacing={2}>
+          {content}
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/helpers/util.js
+++ src/helpers/util.js
@@ -11,3 +11,25 @@ export const getCount = (cart, type = 0) => {
 
   return references ? references.length : 0;
 };
+
+export const daysRemains = (from, to) => {
+  const difference = Math.abs(from - to);
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  if (Object.keys(timeLeft).length === 0) {
+    timeLeft.days = 0;
+    timeLeft.hours = 0;
+    timeLeft.minutes = 0;
+    timeLeft.seconds = 0;
+  }
+  return timeLeft;
+};
